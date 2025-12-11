7a2f1e474d2284900722cd20d2cb04d3cca27631
juneInwza008 <prompiriya.santisookkong@e-tech.ac.th>
2025-12-11 09:36:23 +0700

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import { View, Text, Button, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function App(){
+
+    const router = useRouter()
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.mainTitle}>หน้าแรก</Text>
+            <Button title="ไปหน้า2" onPress={() => router.navigate('/square')}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor:"skyblue",
+        justifyContent: "center",
+       alignItems: "center" 
+      },
+      mainTitle:{
+        fontSize:20,
+        fontWeight: "700"
+      }
+    }
+)
